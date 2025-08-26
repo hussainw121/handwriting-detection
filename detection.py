d3a55bf223690b5c6cea8849c33a65e614430ed5
hussainw121 <hussainwaseem1212@gmail.com>
2025-08-26 14:47:32 +0100

    data formatted

--- detection.py
+++ detection.py
@@ -1,1 +1,27 @@
-print("Hello")
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.utils import to_categorical
+
+train_data = pd.read_csv('Train.csv')
+
+
+X = train_data.iloc[:, 1:]  
+y = train_data.iloc[:, 0]   
+
+if not isinstance(X, pd.DataFrame):
+    X = pd.DataFrame(X)
+X = X.apply(pd.to_numeric, errors='coerce')
+X = X.fillna(0)  
+X = X.values / 255.0
+X = X.reshape(-1, 28, 28, 1)
+
+print("Shape of X after reshaping:", X.shape)
+y = to_categorical(y, num_classes=10)
+print("Shape of y after one-hot encoding:", y.shape)
+
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
+print("X_train shape:", X_train.shape)
